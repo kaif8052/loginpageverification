2c088b9cfd6ad59f4ae4e6c9f857d15e094abb8b
MOHD.KAIF <121995804+kaif8052@users.noreply.github.com>
2024-06-13 15:45:49 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,19 @@
+
+const num1 =12;
+const num2 = 11;
+const num3 =1;
+let largest;
+
+// check the condition
+if(num1 >= num2 && num1 >= num3) {
+    largest = num1;
+}
+else if (num2 >= num1 && num2 >= num3) {
+    largest = num2;
+}
+else {
+    largest = num3;
+}
+
+// display the result
+console.log("The largest number is " + largest);
